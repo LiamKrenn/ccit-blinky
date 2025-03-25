dcd7103fe0191f477dcf1fcc61bc9d6b9854d637
LiamKrenn <k.liam1224@gmail.com>
2025-03-25 12:57:40 +0100

    init

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <FastLED.h>
+#include <stdint.h>
+
+#define LED_PIN 2
+#define NUM_LEDS 1
+#define BRIGHTNESS 255
+#define LED_TYPE WS2811
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
+// void wait_for_serial_connection() {
+//   uint32_t timeout_end = millis() + 2000;
+//   Serial.begin(115200);
+//   while(!Serial && timeout_end > millis()) {}  //wait until the connection to
+//   the PC is established
+
+// }
+
+void setup() {
+  // wait_for_serial_connection(); // Optional, but seems to help Teensy out a
+  // lot.
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS)
+      .setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
+}
+
+void loop() {
+  leds[0] = CRGB::Red;
+  FastLED.show();
+  delay(10);
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(10);
+}
